c08284d8dc286a4e7284ed96822bc43758514374
RYAN <RTIPPER44@GMAIL.COM>
2021-01-04 12:56:01 -0800

    completed refactoring for Lab 7

--- server.js
+++ server.js
@@ -14,46 +14,57 @@ app.use(cors());
 // Routes
 app.get('/', homeHandler);
 app.get('/location', locationHandler);
-app.get('/restaurants', restaurantHandler);
+app.get('/weather', weatherHandler);
 app.use('*', notFoundHandler);
 
 // Function Handlers
 function locationHandler(request, response) {
-  const location = require('./data/location.json');
-  const city = request.query.city;
-  const locationData = new Location(city, location);
-
-  response.send(locationData);
+  let city = request.query.city;
+  let key = process.env.GEOCODE_API_KEY;
+  const url = `https://us1.locationiq.com/v1/search.php?key=${key}&q=${city}&format=json`;
+  superagent.get(url)
+    .then( data => {
+      // Objects Created Baised on Constructor
+      const locationData = data.body[0];
+      const location = new Location(city, locationData);
+      response.status(200).send(location);
+    });
 }
 
 function homeHandler(request, response) {
-  response.send('Hello World!');
+  response.status(200).send('hello world');
 }
 
 function notFoundHandler(request, response) {
   response.send('404, Sorry!');
 }
 
-function restaurantHandler(request, response) {
-  const data = require('./data/weather.json');
-  const weatherArr = [];
-  data.data.forEach(weather => {
-    weatherArr.push(new Weather(weather));
-  });
-  response.send(weatherArr);
+function weatherHandler(request, response) {
+  let key = process.env.WEATHER_API_KEY;
+  let city = request.query.city;
+  const url = `https://api.weatherbit.io/v2.0/current?city=${city}&key=${key}`;
+  superagent.get(url)
+    .then(data => {
+      console.log(data.body);
+      // Objects Created Baised on Constructor
+      // console.log(data.body);
+      const weatherData = data.body[0];
+      const weather = new Weather(city, weatherData);
+      response.status(200).send(weather);
+    });
 }
 
 // Constructor
 function Location(city, geoData) {
   this.search_query = city;
-  this.formatted_query = geoData[0].display_name;
-  this.latitude = geoData[0].lat;
-  this.longitude = geoData[0].lon;
+  this.formatted_query = geoData.display_name;
+  this.latitude = geoData.lat;
+  this.longitude = geoData.lon;
 }
 
-function Weather(data) {
-this.forecast = data.weather.description;
-this.time = data.valid_date;
+function Weather(result) {
+  this.time = result.ob_time;
+  this.forecast = result.data.weather.description;
 }
 
 // Start Server
