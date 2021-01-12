9d3afe99536384de67103866ab136c66f49b62af
RYAN <RTIPPER44@GMAIL.COM>
2021-01-11 21:11:39 -0800

    Lab9 updates - adding in movies

--- server.js
+++ server.js
@@ -20,10 +20,11 @@ app.use(cors());
 app.get('/', requestMain);
 app.get('/location', locationHandler);
 app.get('/weather', weatherHandler);
+app.get('/movies', moviesHandler);
+app.get('/yelp', yelpHandler);
 app.use('*', notFoundRequest);
 
 // Function Handlers
-
 // REQUESTS / ERROR CATCH
 function requestMain(request, response) {
   response.send('hello world');
@@ -68,62 +69,102 @@ function checkLocationData(city, response, url) {
       console.log(error);
     });
 
-  }
+}
 
-  // WEATHER
-  function weatherHandler(request, response) {
-    const key = process.env.WEATHER_API_KEY;
-    const lat = request.query.latitude;
-    const lon = request.query.longitude;
-    const url = `http://api.weatherbit.io/v2.0/forecast/daily?&lat=${lat}&lon=${lon}&key=${key}`;
+// WEATHER
+function weatherHandler(request, response) {
+  const key = process.env.WEATHER_API_KEY;
+  const lat = request.query.latitude;
+  const lon = request.query.longitude;
+  const url = `http://api.weatherbit.io/v2.0/forecast/daily?&lat=${lat}&lon=${lon}&key=${key}`;
 
-    if (flagTrigger) {
-      inputInval(response);
-    } else {
-      superagent.get(url)
-        .then(promise => {
-          // Objects Created Baised on Constructor
-          const testData = promise.body.data.map(val => {
-            return new Weather(val);
-          });
-          response.send(testData);
-        }).catch(error => {
-          console.log(error);
+  if (flagTrigger) {
+    inputInval(response);
+  } else {
+    superagent.get(url)
+      .then(promise => {
+        // Objects Created Baised on Constructor
+        const testData = promise.body.data.map(val => {
+          return new Weather(val);
         });
-    }
-  }
-
-  // MOVIES
-
-  // YELP
-
-  // Helper Functions
-  function inputInval(send) {
-    flagTrigger = true;
-    return send.status(500).send('Sorry, something went wrong');
-  }
-
-  // Constructors
-  function Location(city, data) {
-    this.search_query = city;
-    this.formatted_query = data.display_name;
-    this.latitude = data.lat;
-    this.longitude = data.lon;
-  }
-
-  function Weather(data) {
-    this.time = new Date(data.valid_date).toDateString();
-    this.forecast = data.weather.description;
-  }
-
-  // Start Server
-  client.connect()
-    .then(() => {
-      app.listen(PORT, () => {
-        console.log(`now listening on port,${PORT}`);
-        console.log(`Connected to database ${client.connectionParameters.database}`);
+        response.send(testData);
+      }).catch(error => {
+        console.log(error);
       });
-    })
-    .catch(error => {
-      console.log(error);
+  }
+}
+
+// MOVIES
+function moviesHandler(request, response) {
+  const key = process.env.MOVIES_API_KEY;
+
+  const queryObj = {
+    api_key: key,
+    query: request.query.search_query,
+    include_adult: false
+  };
+  const url = 'https://api.themoviedb.org/3/search/movie';
+
+  if (flagTrigger) {
+    inputInval(response);
+  } else {
+    superagent.get(url)
+      .query(queryObj)
+      .then(data => {
+        const dataHolder = data.body.results;
+        if (dataHolder > 20) {
+          dataHolder.length = 20;
+        }
+        const movieHolder = dataHolder.map(val => {
+          return new Movies(val);
+        });
+
+        response.status(200),json(movieHolder);
+      }).catch(error => {
+        console.log(error);
+      });
+  }
+}
+
+// YELP
+
+// Helper Functions
+function inputInval(send) {
+  flagTrigger = true;
+  return send.status(500).send('Sorry, something went wrong');
+}
+
+// Constructors
+function Location(city, data) {
+  this.search_query = city;
+  this.formatted_query = data.display_name;
+  this.latitude = data.lat;
+  this.longitude = data.lon;
+}
+
+function Weather(data) {
+  this.time = new Date(data.valid_date).toDateString();
+  this.forecast = data.weather.description;
+}
+
+function Movies(data) {
+  this.title = data.original_title;
+  this.overview = data.overview;
+  this.average_votes = data.vote_average;
+  this.total_votes = data.vote_count;
+  this.image_url = `https://image.tmdb.org/t/p/w500${data.poster_path}`;
+  this.popularity = data.popularity;
+  this.released_on = data.release_date;
+}
+
+// Start Server
+client.connect()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`now listening on port,${PORT}`);
+      console.log(`Connected to database ${client.connectionParameters.database}`);
     });
+  })
+  .catch(error => {
+    console.log(error);
+  });
